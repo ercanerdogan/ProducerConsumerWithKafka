a795258131193c6a0ba7b2c198a1493ad5071dc5
ercanerdogan@gmail.com <ercanerdogan@gmail.com>
2020-12-20 06:02:45 +0300

    -KafkaProducerHostedService -KafkaConsumerHostedService
    services were implemented.

---
+++ ProducerConsumer/ProducerConsumerApp/Program.cs
@@ -0,0 +1,107 @@
+﻿using Confluent.Kafka;
+using Kafka.Public;
+using Kafka.Public.Loggers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProducerConsumerApp
+{
+    public class Program 
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureServices((context, collection) =>
+            {
+                collection.AddHostedService<KafkaConsumerHostedService>();
+                collection.AddHostedService<KafkaProducerHostedService>();
+
+
+            });
+    }
+
+    public class KafkaProducerHostedService : IHostedService
+    {
+        private readonly ILogger<KafkaProducerHostedService> _logger;
+        private IProducer<Null, string> _producer;
+
+        public KafkaProducerHostedService(ILogger<KafkaProducerHostedService> logger)
+        {
+            _logger = logger;
+            var config = new ProducerConfig()
+            {
+                BootstrapServers = "kafka:9092",
+                ClientId = Dns.GetHostName()
+            };
+
+            _producer = new ProducerBuilder<Null, string>(config).Build();
+
+        }
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var value = $"Publish : Hello Kafka world {i}";
+                _logger.LogInformation(value);
+
+                await _producer.ProduceAsync("KafkaDemo", new Message<Null, string>()
+                {
+                    Value = value
+                }, cancellationToken);
+            }
+            _producer.Flush(TimeSpan.FromSeconds(10));
+            
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _producer?.Dispose();
+            return Task.CompletedTask;
+
+        }
+    }
+
+    public class KafkaConsumerHostedService : IHostedService
+    {
+        private readonly ILogger<KafkaConsumerHostedService> _logger;
+        private ClusterClient _cluster;
+
+        public KafkaConsumerHostedService(ILogger<KafkaConsumerHostedService> logger)
+        {
+            _logger = logger;
+            _cluster = new ClusterClient(new Configuration
+            {
+                Seeds ="kafka:9092"
+
+            }, new ConsoleLogger());
+
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _cluster.ConsumeFromLatest("KafkaDemo");
+            _cluster.MessageReceived += record =>
+            {
+                _logger.LogInformation($@"Received : {Encoding.UTF8.GetString(record.Value as byte[])}");
+            };
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _cluster?.Dispose();
+            return Task.CompletedTask;
+        }
+    }
+}
